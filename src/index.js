5426bf22c0c351ff6c35f21b3e1eafe09759d27c
jaridwarren <jaridwarren@gmail.com>
2018-10-10 14:45:23 -0700

    add search boilerplate, add YouTube API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/searchbar';
+
+const API_KEY = 'AIzaSyAqUpMZhYgIdsT8MZb0qW8TYH0a4mjs9Qw';
+
 // Create a new component. This component should produce some HTML
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 // Take this component's generated HTML and place in the DOM
