f98455e43e2b05063450c8a934864554e9d2da80
jaridwarren <jaridwarren@gmail.com>
2018-10-10 20:23:09 -0700

    can now render a video detail, video list, and select a new video

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAqUpMZhYgIdsT8MZb0qW8TYH0a4mjs9Qw';
 
-// Create a new component. This component should produce some HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-// Take this component's generated HTML and place in the DOM
+    YTSearch({ key: API_KEY, term: 'Sidney Crosby' }, videos => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
