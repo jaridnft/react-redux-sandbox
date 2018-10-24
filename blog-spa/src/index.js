b4353f4b87804e46f7254675b8e6438a59f085e5
jaridwarren <jaridwarren@gmail.com>
2018-10-24 11:59:31 -0700

    change name of blog app

---
+++ blog-spa/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class Hello extends React.Component {
+  render() {
+    return <div>Hello!</div>;
+  }
+}
+
+class Goodbye extends React.Component {
+  render() {
+    return <div>Goodbye!</div>;
+  }
+}
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.container')
+);
