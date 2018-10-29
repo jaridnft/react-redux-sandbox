91769ac6e9605ebd698ef9ee51def16d367507eb
jaridwarren <jaridwarren@gmail.com>
2018-10-28 21:36:28 -0700

    hooked up submit button and form error handling

--- blog-spa/src/components/posts_new.js
+++ blog-spa/src/components/posts_new.js
@@ -2,22 +2,73 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
-        <input type="text" {...field.input} />
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
       </div>
     );
   }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
-        <Field name="title" component={this.renderTitleField} />
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        {/* Field is a component of redux-form */}
+        <Field
+          label="Title of Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     );
   }
 }
 
+// values is information submitted to form
+// called automatically with redux-form on submission
+function validate(values) {
+  const errors = {};
+
+  if (!values.title || values.title.length < 3) {
+    // NOTE: property name must match 'name' property in Field component
+    errors.title = "Enter a title that's at least 3 characters!";
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter a category!';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter some content!';
+  }
+
+  // if errors is empty, the form is fine to submit
+  // if errors has non-zero properties, redux-form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
--- blog-spa/src/reducers/index.js
+++ blog-spa/src/reducers/index.js
@@ -4,7 +4,7 @@ import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  forms: formReducer
+  form: formReducer
 });
 
 export default rootReducer;
